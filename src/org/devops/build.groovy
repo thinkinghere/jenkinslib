81ba3c3834f10ce2cab7ca9aef925b9a93c19ae3
thinkinghere <44504696+thinkinghere@users.noreply.github.com>
2020-12-28 09:42:35 +0800

    Create build.groovy

---
+++ src/org/devops/build.groovy
@@ -0,0 +1,10 @@
+package org.devops
+
+def Buidld(buidType, buildShell) {
+  def buildTools = ["mvn":"M2", "ant":"ANT", "gradle":"GRADLE", "npm":"NPM"] // 定义映射
+  
+  println("当前的构建类型是: ${buidType}")
+  buildHome = tool buildTools[buidType]
+  
+  sh "${buildHome}/bin/${buidType} ${buildShell}"
+}
